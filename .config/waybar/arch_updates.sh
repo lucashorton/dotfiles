57ee9afaf623ee115aa6129a2dc23416d3d36094
Lucas Horton <lucas@lucashorton.com>
2021-09-01 11:52:58 -0500

    Removing arch update module

--- .config/waybar/arch_updates.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-UPDATES=$(yay -Qu | wc -l)
-
-if [ $UPDATES -gt 0 ]; then
-  echo "{\"text\":\"$UPDATES\", \"alt\": \"available\"}"
-elif [ $UPDATES -eq 0 ]; then
-  echo '{"text":"", "alt":"none"}'
-fi
