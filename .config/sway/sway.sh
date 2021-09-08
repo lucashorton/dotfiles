81af0908099b8c51a94642186eede15f3dabb89e
Lucas Horton <lucas@lucashorton.com>
2021-09-08 01:10:28 -0500

    Reorganizing

--- .config/sway/sway.sh
+++ .config/sway/sway.sh
@@ -2,9 +2,9 @@
 
 # Launch sway 
 if [ $HOSTNAME = "pele" ]; then
-  sway -c $HOME/.config/sway/laptop-config
+  sway -c $HOME/.config/sway/laptop.conf
 else
-  sway -c $HOME/.config/sway/desktop-config
+  sway -c $HOME/.config/sway/desktop.conf
 fi
 
 playerctld daemon
