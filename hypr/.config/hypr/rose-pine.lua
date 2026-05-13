9d9b4886aaff4ccabc9825a352836f3a0e22fc56
Lucas Horton <lucas@lucashorton.com>
2026-05-12 21:52:41 -0500

    Converting to lua

---
+++ hypr/.config/hypr/rose-pine.lua
@@ -0,0 +1,19 @@
+-- name: Rosé Pine
+-- author: lucashorton
+-- upstream: https://github.com/lucashorton/hyprland-rosepine/blob/main/rose-pine.conf
+-- All natural pine, faux fur and a bit of soho vibes for the classy minimalist
+base = "191724"
+surface = "1f1d2e"
+overlay = "26233a"
+muted = "6e6a86"
+subtle = "908caa"
+text = "e0def4"
+love = "eb6f92"
+gold = "f6c177"
+rose = "ebbcba"
+pine = "31748f"
+foam = "9ccfd8"
+iris = "c4a7e7"
+highlightLow = "21202e"
+highlightMed = "403d52"
+highlightHigh = "524f67"
